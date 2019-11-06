bbb1f99d575e35e97fdc898c02a08189a25b2da9
Yehlo <joshua.leuenberger@outlook.de>
2019-11-06 17:00:03 +0100

    fixed script

--- myScripts/Write-RemoteMessage.ps1
+++ myScripts/Write-RemoteMessage.ps1
@@ -1,10 +1,10 @@
-$cred = get-credential
+$cred = get-credentials
 
 $liste = 10, 3,4,9,8,6,1,2 | ForEach-Object { 'CL{0:d2}' -f $_ }
 $liste = $liste -ne $env:COMPUTERNAME
 
 Invoke-Command {
     $GetUserName = [Environment]::UserName
-    $CmdMessage = {C:\windows\system32\msg.exe * 'Hello'}
+    $CmdMessage = {C:\windows\system32\msg.exe * 'Hello' $GetUserName 'This is a test!'}
     $CmdMessage | Invoke-Expression
 } -ThrottleLimit 20 -ComputerName $liste -Credential $cred 
